0d872218773803d6ebeb33357c009e25a572debd
unknown <you@example.com>
2019-09-25 09:40:23 +0300

    Created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/PageSchedule.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PageSchedule
   }
 }
 </script>
@@ -23,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ src/components/PageSchedule.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="schedule">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PageSchedule',
+}
+</script>
+
+<style scoped lang="scss">
+.schedule {
+  margin: 45px 0 0 41px;
+  font-family: 'Rubik', 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 17px;
+  text-align: left;
+  border: 1px solid black;
+}
+
+</style>
